fcd53b95a6c256178010dd9adf352df18372e5d2
Allan <allan.miljan@tptlive.ee>
2026-05-16 11:06:14 +0300

    fix code

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PublicController extends Controller
 {
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,9 +2,6 @@
 @section('title', 'Home page')
 @section('content')
     <div class="card bg-base-300 shadow-sm">
-        <figure>
-            <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
-        </figure>
         <div class="card-body">
             <h2 class="card-title">{{ $post->title }}</h2>
             <p>{!! $post->displayBody !!}</p>
@@ -12,5 +9,4 @@
             </div>
         </div>
     </div>
-    @endforeach
 @endsection
