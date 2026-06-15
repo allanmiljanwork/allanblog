152154c8cc38ddd195ed4ddec9a970dc4d86e77c
Allan Miljan <allan.miljan@tptlive.ee>
2026-06-15 18:34:02 +0300

    add check if image exist and is an image

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -35,10 +35,12 @@ class PostController extends Controller
         $post->user()->associate(Auth::user());
         $post->save();
 
-        $image = new Image();
-        $image->pic = $request->file('image')->store('', 'public');
-        $image->post()->associate($post);
-        $image->save();
+        if ($request->file('image')) {
+            $image = new Image();
+            $image->pic = $request->file('image')->store('', 'public');
+            $image->post()->associate($post);
+            $image->save();
+        }
 
         return redirect()->route('posts.index');
     }
